719440ec097af3e96c5df67340129516589ec97f
Vinicius Mendite <vinicius.miff@gmail.com>
2020-11-06 16:55:09 -0300

    eslint and prettier configured

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  response.json({ message: 'Hello Vinícius!' })
-})
+  return response.json({ message: 'Hello Vinícius!' });
+});
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!')
-})
+  console.log('Server started on port 3333!');
+});
