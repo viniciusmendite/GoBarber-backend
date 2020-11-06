ff0ea2a5bffeba7023c143fc56f59e31eecf3353
Vinicius Mendite <vinicius.miff@gmail.com>
2020-11-06 16:12:23 -0300

    configure typescript and ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello Vinícius!' })
+})
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!')
+})
