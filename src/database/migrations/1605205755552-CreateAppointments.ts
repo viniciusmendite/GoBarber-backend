991af17fce72350c9bfb973a773c47129cdf9b49
Vinicius Mendite <vinicius.miff@gmail.com>
2020-11-13 22:03:35 -0300

    create model user, add field in migration appointment and model appointment

--- src/database/migrations/1605205755552-CreateAppointments.ts
+++ src/database/migrations/1605205755552-CreateAppointments.ts
@@ -22,6 +22,16 @@ export default class CreateAppointments1605205755552
             name: 'date',
             type: 'timestamp with time zone',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
