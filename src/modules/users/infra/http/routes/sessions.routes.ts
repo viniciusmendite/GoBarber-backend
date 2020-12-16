7a4b1877f50129656a5141ee078eb89da20f647c
Vinicius Mendite <vinicius.miff@gmail.com>
2020-12-16 09:35:58 -0300

    create controller for routes

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,26 +1,8 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
-
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const authenticateUser = container.resolve(AuthenticateUserService);
-
-  const { user, token } = await authenticateUser.execute({ email, password });
-
-  const userWithoutPassword = {
-    avatar: user.avatar,
-    id: user.id,
-    name: user.name,
-    email: user.email,
-    created_at: user.created_at,
-    updated_at: user.updated_at,
-  };
-
-  return response.json({ userWithoutPassword, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 export default sessionsRouter;
