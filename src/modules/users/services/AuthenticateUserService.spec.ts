6118a753602fdc5acaf6d5e3ba0de1ead7f872c0
Vinicius Mendite <vinicius.miff@gmail.com>
2020-12-16 15:37:18 -0300

    create provider hash bcryptjs

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,25 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+
+describe('AuthenticateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+
+    const createUser = new CreateUserService(fakeUsersRepository);
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+  });
+});
