a72e78f0b4fdf07e49f87256e6593c4b9dc9f955
Vinicius Mendite <vinicius.miff@gmail.com>
2021-01-22 16:00:40 -0300

    using class-transformer

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
@@ -11,16 +12,7 @@ class SessionsController {
 
     const { user, token } = await authenticateUser.execute({ email, password });
 
-    const userWithoutPassword = {
-      avatar: user.avatar,
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json({ userWithoutPassword, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
 
